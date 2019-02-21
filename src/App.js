d8300d85abdeb701bec5fc4adfc10d5c716aef1c
Sebastian <sebastian.leonowicz5@gmail.com>
2019-02-21 13:30:50 +0100

    creates an arrqay of react elements and pushes new array into state

--- src/App.js
+++ src/App.js
@@ -5,31 +5,50 @@ const axios = require('axios');
 
 class App extends Component {
 
+  state = {
+    componentArray: []
+  }
+
   componentDidMount() {
     axios.get('https://api.github.com/orgs/quandoo/repos')
       .then((response) => {
-        console.log(response);
+        this.createArrayOfComponents(response.data);
       })
+      .catch((error) => {
+        console.log('Whoops! Something went wrong :(', error);
+      });
   }
+
+  createArrayOfComponents (data) {
+    console.log(data);
+    const componentArray = data.map((data) => {
+      return  <div>
+                <ul>
+                  <li>
+                    <p>RepoName</p>
+                    <span>Forked From</span>
+                    <span>Description</span>
+                    <ul>
+                      <li>language</li>
+                      <li>number of forks</li>
+                      <li>appliable legal MIT/else</li>
+                      <li>last update</li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+    });
+    console.log('new data', componentArray);
+    this.setState({
+      componentArray: componentArray
+    })
+    console.log(this.state);
+  }
+
 
   render() {
     return (
       <div className="App">
-        <div>
-          <ul>
-            <li>
-              <p>RepoName</p>
-              <span>Forked From</span>
-              <span>Description</span>
-              <ul>
-                <li>language</li>
-                <li>number of forks</li>
-                <li>appliable legal MIT/else</li>
-                <li>last update</li>
-              </ul>
-            </li>
-          </ul>
-        </div>
         <div>Component 2</div>
       </div>
     );
