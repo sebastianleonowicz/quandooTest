0bbab9c31481b59cb5bf804fc1e81d09ba4f0dac
Sebastian <sebastian.leonowicz5@gmail.com>
2019-02-21 13:14:51 +0100

    adds first html markup for the list component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const axios = require('axios');
+
 class App extends Component {
 
   componentDidMount() {
-
+    axios.get('https://api.github.com/orgs/quandoo/repos')
+      .then((response) => {
+        console.log(response);
+      })
   }
 
   render() {
     return (
       <div className="App">
-        <div>Component 1</div>
+        <div>
+          <ul>
+            <li>
+              <p>RepoName</p>
+              <span>Forked From</span>
+              <span>Description</span>
+              <ul>
+                <li>language</li>
+                <li>number of forks</li>
+                <li>appliable legal MIT/else</li>
+                <li>last update</li>
+              </ul>
+            </li>
+          </ul>
+        </div>
         <div>Component 2</div>
       </div>
     );
