690ddd1e238aee31fe1a0692319e5d3f6060bf06
Sebastian <sebastian.leonowicz5@gmail.com>
2019-02-21 12:56:20 +0100

    intalled axios, sets the project for working

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+
+  }
+
   render() {
     return (
       <div className="App">
-        <p>some random</p>
+        <div>Component 1</div>
+        <div>Component 2</div>
       </div>
     );
   }
